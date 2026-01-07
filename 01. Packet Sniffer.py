4ab37fd091a0ea7a6f2a60119d05ecf8a3fc6407
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2026-01-07 18:14:26 +0000

    Update 01. Packet Sniffer.py
    Added main() function to capture packets. This is a separate branch as the socket.AF_PACKET does not work on non-Windows systems and therefore I cannot verify that it runs without testing on a VM first

--- 01. Packet Sniffer.py
+++ 01. Packet Sniffer.py
@@ -5,13 +5,24 @@ import socket
 import struct
 import textwrap
 
+def main():  # Runs forever, captures data packets, then calls the other functions to handle them
+    connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))  # NOTE: AF_PACKET will not work on non-Linux operating systems
+    while True:
+        rawData, address = conn.recvfrom(65535)
+        destMac, sourceMac, ethProto, data = ethernetFrame(rawData)
+        
+        # The next two lines are used for debugging purposes. As I am not on a Linux system, I cannot run this program as it currently is. However, I am going to test this on a Linux system to verify it works
+        print("\nEthernet frame: ")
+        print("Destination: {}, Source: {}, Protocol: {}".format(destMac, sourceMac, ethProto))
+
 # Unpack ethernet frame
 def ethernetFrame(data):
     destMac, sourceMac, proto = struct.unpack('! 6s 6s H', data[:14])  # Standardise the first 14 parts of input as 6 bytes, 6 bytes, small unsigned int and store in 3 variables
     return getMacAddress(destMac), getMacAddress(sourceMac), socket.htons(proto), data[14:]
 
-# Properly format MAC address into upper case hexadecimal as opposed to bytes
+# Properly format MAC address into upper case hexadecimal as opposed to bytes 
 def getMacAddress(bytesAddress):
     bytesString = map('{:02x}'.format, bytesAddress)
     return ':'.join(bytesString).upper()
     
+main()
